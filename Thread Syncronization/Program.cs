8e64ad19f36c837597279919c3abd0fd28c7ee86
khaled Ibrahiem <khaled.ibrahiem@msa.edu.eg>
2026-06-07 23:44:46 +0300

    v3

---
+++ Thread Syncronization/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Thread_Syncronization
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            System.Threading.Lock lo = new Lock();
+            Thread thread1 = new Thread(IncrementCounter);
+            Thread thread2 = new Thread(IncrementCounter);
+            thread1.Start();
+            thread2.Start();
+
+            thread1.Join();
+            thread2.Join();
+
+            Console.WriteLine(count);
+
+
+            void IncrementCounter()
+            {
+                lock (lo)
+                {
+                    for (int i = 0; i < 100000; i++)
+                    {
+                        count++;
+                    }
+                }
+            }
+        }
+    }
+}
