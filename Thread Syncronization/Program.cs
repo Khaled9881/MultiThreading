2d5df258e4dbc23dfb7b7689c9a8c6934164b1b2
khaled Ibrahiem <khaled.ibrahiem@msa.edu.eg>
2026-06-09 12:31:18 +0300

    v3

--- Thread Syncronization/Program.cs
+++ Thread Syncronization/Program.cs
@@ -1,30 +1,68 @@
-﻿namespace Thread_Syncronization
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Thread_Syncronization
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int count = 0;
-            System.Threading.Lock lo = new Lock();
-            Thread thread1 = new Thread(IncrementCounter);
-            Thread thread2 = new Thread(IncrementCounter);
-            thread1.Start();
-            thread2.Start();
+            //int count = 0;
+            //System.Threading.Lock lo = new Lock();
+            //Thread thread1 = new Thread(IncrementCounter);
+            //Thread thread2 = new Thread(IncrementCounter);
+            //thread1.Start();
+            //thread2.Start();
 
-            thread1.Join();
-            thread2.Join();
+            //thread1.Join();
+            //thread2.Join();
 
-            Console.WriteLine(count);
+            //Console.WriteLine(count);
 
 
-            void IncrementCounter()
+            //void IncrementCounter()
+            //{
+            //    lock (lo)
+            //    {
+            //        for (int i = 0; i < 100000; i++)
+            //        {
+            //            count++;
+            //        }
+            //    }
+            //}
+
+
+
+            Dictionary<int, string> map = new Dictionary<int, string>();
+            ReaderWriterLockSlim _lock = new ReaderWriterLockSlim();
+
+
+            void add(int key, string value)
             {
-                lock (lo)
+                bool lockAcquired = false;
+                try
                 {
-                    for (int i = 0; i < 100000; i++)
-                    {
-                        count++;
-                    }
+                    _lock.EnterWriteLock();
+                    lockAcquired = true;
+                    map[key] = value;
+                }
+                finally
+                {
+                    if (lockAcquired) _lock.ExitWriteLock();
+                }
+            }
+
+            string get(int key)
+            {
+                bool lockAcquired = false;
+                try
+                {
+                    _lock.EnterReadLock();
+                    lockAcquired = true;
+                    return map[key];
+                }
+                finally
+                {
+                    if (lockAcquired) _lock.ExitReadLock();
                 }
             }
         }
